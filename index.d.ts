cd97152acb00a86447ca93c87ae2ae7dd7ada38b
Agam-Deep-AJ <agam.deep@oneredmaple.com>
2023-05-24 14:18:31 -0400

    Added typescript declaration for module and updated package.json to bundle the declaration file with the module

---
+++ index.d.ts
@@ -0,0 +1,17 @@
+declare module "orm-logs-module" {
+
+    export const LEVELS: {
+      VERBOSE: number,
+      INFO: number,
+      WARN: number,
+      ERROR: number,
+      CRITICAL: number
+    };
+
+    export function init(processName: any, hostLocation: any): void;
+
+    export function getLevelNameByValue(value: any): any;
+
+    export function log(msg: any, options?: { level?: any, tags?: any }): void;
+
+  }
